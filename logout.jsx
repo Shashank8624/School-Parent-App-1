257a5ccc44324857c0eadb83f3d30fd98ed7ca59
Shashank8624 <85009947+Shashank8624@users.noreply.github.com>
2024-06-10 10:36:19 +0530

    Update

---
+++ logout.jsx
@@ -0,0 +1,27 @@
+// AuthContext.jsx
+import React, { createContext, useState, useContext } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userType, setUserType] = useState('');
+
+  const login = (type) => {
+    setIsAuthenticated(true);
+    setUserType(type);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    setUserType('');
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout, userType }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
