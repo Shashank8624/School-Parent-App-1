e368fccaa09072806f3cd4d05e1501b53c5f0c28
Shashank8624 <85009947+Shashank8624@users.noreply.github.com>
2024-06-13 17:17:03 +0530

    Update CreateNewCircular.js

--- CreateNewCircular.js
+++ CreateNewCircular.js
@@ -84,3 +84,16 @@ export default function CreateNewCircular() {
 
 [PrivateRoute] is not a <Route> component. All component children of <Routes> must be a <Route> or <React.Fragment>
   
+
+// src/components/PrivateRoute.js
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+
+const PrivateRoute = ({ children }) => {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
