8161ee898564475e69fdd9e6a32289abc0715931
Shashank8624 <85009947+Shashank8624@users.noreply.github.com>
2024-06-13 16:18:42 +0530

    Update logout.jsx

--- logout.jsx
+++ logout.jsx
@@ -268,4 +268,24 @@ Can't resolve './AuthContext' in 'C:\School App\school-app\src\cmp'
 
 ///////////////////// 13-06-24////////////////////////////////////////////////////
 
+import React, { createContext, useState } from 'react';
 
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => {
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
