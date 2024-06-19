763ddedf6e0d5612ce52c514544a9e70945291b2
Shashank8624 <85009947+Shashank8624@users.noreply.github.com>
2024-06-19 16:28:32 +0530

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -138,3 +138,67 @@ export const Navbar = () => {
     </nav>
   );
 };
+
+
+ex-03
+// src/cmp/Navbar.js
+
+import React, { useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import "./Navbar.css";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
+
+  // Function to determine if current page is ParentDashboard
+  const isParentDashboard = location.pathname === "/ParentDashboard";
+
+  return (
+    <nav>
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li>
+          <NavLink exact to="/" activeClassName="active">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" activeClassName="active">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/holidayList" activeClassName="active">
+            Holiday List
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/feeDetails" activeClassName="active">
+            FeeDetails
+          </NavLink>
+        </li>
+        {isParentDashboard && ( // Only show "Circular" link on ParentDashboard page
+          <li>
+            <NavLink to="/circular" activeClassName="active">
+              Circular
+            </NavLink>
+          </li>
+        )}
+      </ul>
+      <ul>
+        <li>
+          <NavLink to="/register" activeClassName="active">
+            Register
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
